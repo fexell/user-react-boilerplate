9cec79c80e591900bcdd6564a345f8df2915969b
Felix Cervin <fexellofficial@gmail.com>
2025-10-26 17:05:05 +0100

    fix: Removed block from form Button.component.jsx

--- src/auth/components/Form/Button.component.jsx
+++ src/auth/components/Form/Button.component.jsx
@@ -9,7 +9,7 @@ const AuthFormButtonComponent               = ({ buttonType, buttonText, isButto
           <button
             className={
               clsx(
-                'block flex flex-row w-full justify-center p-4 mb-4 text-white rounded-full',
+                'flex flex-row w-full justify-center p-4 mb-4 text-white rounded-full',
                 'bg-blue-600 hover:bg-blue-700 cursor-pointer disabled:hover:bg-blue-600 disabled:cursor-not-allowed disabled:opacity-50',
                 'shadow-2xl shadow-blue-600/50 dark:shadow-2xl dark:shadow-blue-600/50'
               )
